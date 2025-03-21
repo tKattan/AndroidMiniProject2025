969f46f6500efa9d05ca0ff9ab9ef3a0b66e27b4
dufrechou <laurian.dufrechou@gmail.com>
2025-03-21 12:50:30 +0100

    adding sensor accelerometer to SensorRepository.java

--- app/src/main/java/com/example/androidminiproject2025/SensorRepository.java
+++ app/src/main/java/com/example/androidminiproject2025/SensorRepository.java
@@ -1,29 +1,95 @@
 package com.example.androidminiproject2025;
 
+import static android.content.Context.SENSOR_SERVICE;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import androidx.annotation.NonNull;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SensorRepository {
+
+
+    private final Context context;
     private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-    public SensorRepository() {
+    public SensorRepository(Context context) {
+        this.context = context;
     }
 
-    public void checkIfMovementIsGood(String x, RepositoryCallback<Boolean> callback, CancellationToken cancellationToken) {
-        executorService.submit(() -> {
-            while (!cancellationToken.isCancelled()) {
-                // Simulate checking movement
-                boolean isGoodMovement = checkMovement();
-                if (isGoodMovement){
-                    callback.onComplete(new Result.Success<>(true));
-                    return;
+    private static boolean correctInput = false;
+    private static float accelXEtalon;
+    private static float accelYEtalon;
+    private static float accelZEtalon;
+    private static boolean etalonAccelDone = false;
+
+
+    public void checkIfMovementIsGood(String difficulty, RepositoryCallback<Boolean> callback, CancellationToken cancellationToken) {
+        executorService.execute(() -> {
+            SensorManager sensorMan;
+            sensorMan = (SensorManager) context.getSystemService(SENSOR_SERVICE);
+            SensorEventListener sensorEventListener = getSensorEventListener();
+            try {
+                PackageManager manager = context.getPackageManager();
+                Sensor accelerometer = sensorMan.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                sensorMan.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_UI);
+
+                while (!cancellationToken.isCancelled() || !correctInput) {
+
                 }
+                callback.onComplete(new Result.Success<>(true));
+            } finally {
+                sensorMan.unregisterListener(sensorEventListener);
             }
         });
     }
 
-    private boolean checkMovement() {
-        // Simulate movement check logic
-        return Math.random() > 0.5;
+
+    @NonNull
+    private static SensorEventListener getSensorEventListener() {
+        SensorEventListener sensorEventListener = null;
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    correctInput = computeAccelerometerValues(event.values);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        return sensorEventListener;
     }
+
+    private static boolean computeAccelerometerValues(float[] values){
+        if(etalonAccelDone){
+            if(accelXEtalon -5 > values[0] ||accelXEtalon +5 < values[0]){
+                return true;
+            }
+            if(accelYEtalon -5 > values[1] ||accelYEtalon +5 < values[1]){
+                return true;
+            }
+            if(accelZEtalon -5 > values[2] ||accelZEtalon +5 < values[2]){
+                return true;
+            }
+            return false;
+        }else{
+            accelXEtalon = values[0];
+            accelYEtalon = values[1];
+            accelZEtalon = values[2];
+            etalonAccelDone = true;
+            return false;
+        }
+    }
+
 }
